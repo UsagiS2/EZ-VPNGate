3bfc84f5b5f247105791a04ad16e1af8e7f94441
UsagiS2 <tupvct@gmail.com>
2016-03-17 00:21:18 +0700

    init

---
+++ main/src/main/java/com/usagis2/ezvpngate/activities/DashBoard.java
@@ -0,0 +1,14 @@
+package com.usagis2.ezvpngate.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import de.blinkt.openvpn.R;
+
+public class DashBoard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+}
